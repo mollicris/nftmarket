7156945febf3b1805bef5207f9b3b01702d5e221
cmollinedo <cristianm184@gmail.com>
2025-07-29 00:02:24 -0400

    Adicion de control Upload

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -8,14 +8,55 @@ import { useAccount } from "wagmi";
 import { Address } from "~~/components/scaffold-eth";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-eth";
 
+//type SelectedFile = File | null;
+
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
   const [files, setFiles] = useState([]);
+  //const [selectedFile,setSelectedFile] = useState<Blob>();
   const server = process.env.NEXT_PUBLIC_SERVER_URL;
 
   const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract({
     contractName: "YourContract",
   });
+
+  const handleFileChange = (event: any) => {
+    // Obtener el primer archivo seleccionado
+    const file = event.target.files[0];
+    console.log(file);
+    //setSelectedFile(file as Blob);
+  };
+
+  async function upload() {
+    const url = "https://uploads.pinata.cloud/v3/files";
+    const form = new FormData();
+    form.append("network", "public");
+    //form.append('file', selectedFile);
+    // form.append("pinataMetadata",
+    //   JSON.stringify({
+    //     name: selectedFile.name,
+    //   })
+    // )
+    form.append("name", "product");
+
+    const options = {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + process.env.NEXT_PUBLIC_PINATA_JWT,
+      },
+      body: form,
+    };
+
+    //options.body = form;
+
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   const options = {
     method: "GET",
@@ -43,6 +84,11 @@ const Home: NextPage = () => {
     <>
       Address:
       <Address address={connectedAddress}></Address>
+      Subir NFT
+      <input type="file" className="file-input file-input-ghost" onChange={handleFileChange} />
+      <button className="btn" onClick={() => upload()}>
+        UpLoad
+      </button>
       {files.map((file: IPFSFile) => (
         <div className="card bg-base-100 w-96 shadow-sm" key={file.cid}>
           {file.mime_type?.startsWith("image/") && (
